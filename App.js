e88a8dc57357c56d14d1940bbdbf8d759d64eaaa
FrostyDev <91427215+DevAman1@users.noreply.github.com>
2022-01-08 21:05:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import React, { useState } from 'react'
+
+
+function App(){
+  
+    return(
+
+<div class="container">  
+<button id ="btn" align="center"> Buy Token </button>  
+</div> 
+   );
+}
+
+// export default App;
+
+export default App
