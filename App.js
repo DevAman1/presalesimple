e4232b03adfeabd3d40e6669768e84e70ad95178
FrostyDev <91427215+DevAman1@users.noreply.github.com>
2022-01-08 21:36:58 +0530

    Update App.js

--- App.js
+++ App.js
@@ -5,10 +5,29 @@ import React, { useState } from 'react'
 function App(){
   
     return(
-
-<div class="container">  
-<button id ="btn"> Buy Token </button>  
-</div> 
+<>
+      <div className="container my-3">
+      <h1> Presale</h1>
+      <div className="mb-3">
+        <textarea
+          className="form-control"
+          
+          id="mybox"
+          rows="1"
+        ></textarea>
+      </div>
+    
+      <button type="button" className="btn btn-primary mx-2 my-2" >
+        Buy Token
+      </button>
+    </div>
+    <div className="container my-3">
+      <h3> Enter BNB Amount</h3>
+      <p>
+     
+      </p>
+    </div>
+    </>
    );
 }
 
